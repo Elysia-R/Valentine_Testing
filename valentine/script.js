ee7fc3b12ae3fc324e9865e0960ef2b4072255cb
Elysia Rolivia <118583227+Elysia-R@users.noreply.github.com>
2024-02-04 18:13:25 +0700

    Add files via upload

---
+++ valentine/script.js
@@ -0,0 +1,53 @@
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".button");
+const yesButton = document.querySelector(".yes");
+const noButton = document.querySelector(".noo");
+const myImg = document.querySelector(".img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+noButton.addEventListener("click", handleNoClick)
+
+function handleYesClick() {
+  titleElement.innerHTML = "Thank You!!! :)";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+function handleNoClick() {
+  noCount++;
+  titleElement.innerHTML = generateMessage(noCount);
+  buttonsContainer.classList.add("hidden");
+  const imageIndex = Math.min(noCount, MAX_IMAGES);
+  changeImage(imageIndex);
+  if (noCount === MAX_IMAGES) {
+    play = false;
+  }
+}
+
+function changeImage(image) {
+  myImg.src = `img/elysia-${image}.jpg`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "What about now?",
+    "You break my heart :(",
+    "Pleaseeeee :(",
+    "I'm gonna cry...",
+    "Pls press yes",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function toggleMuted() {
+  var sound = document.getElementById('sound');
+  sound.muted = !sound.muted;
+}
